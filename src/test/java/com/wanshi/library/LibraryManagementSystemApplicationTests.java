7c6d93f942bd2d38456f32776501fd592b871e38
ZhangWanshi <wanshizhang77@gmail.com>
2026-02-02 23:36:51 +0000

    Initial Spring Boot project setup

---
+++ src/test/java/com/wanshi/library/LibraryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanshi.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
